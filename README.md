e9d086330752a9e81280a105721dbadac1f84d34
William GENETELLI <william.genetelli@gmail.com>
2026-06-18 12:16:31 +0200

    Updated README with pyvisa explanation for powermeter. Updated acquisition of laser information

--- README.md
+++ README.md
@@ -35,4 +35,21 @@ raw/processed data, and small utility modules that implement common helpers.
   - `plot_oscillo`: Notebook for oscilloscope data plotting;
   - `plot_pm100a`: Notebook for PM100A power meter data plotting;
   - `plot_shg`: Notebook for SHG data plotting;
-  
+
+# PYVISA
+
+Every electronic in the room is connected via PYVISA expect for the OC3 temperature controller which communicates via pyserial and the NKT laser which use the NKT DLL library.
+
+## Thorlabs PM
+
+The thorlabs powermeters sometimes failed to connect via pyvisa, even though they are properly listed in the ressource manager. By default, they use a USB protocole instead of the NI-visa. To change this:
+
+1. Open the software called **Optical Parameter Monitor**
+2. Double click on the rectangle with the device's name (if you do not see any device, add one using the plus sign)
+
+![alt text](image.png)
+
+3. The **driver switcher** pannel should open. You might need to restart it as admin.
+4. Rescan, and click on **switch all to NI-VISA**.
+![alt text](image-1.png)
+5. Go back in the notebook, refresh the `rm = pv.ResourceManager()`, the powermeter should connect via pyvisa now.
--- utils/nkt.py
+++ utils/nkt.py
@@ -39,17 +39,26 @@ def find_nkt_devices(close_after=True):
     return open_ports
 
 
-def read_power_and_temp(port='COM4', harmonik_addr=74, adjustik_addr=128):
-    """Read power (W) and temperature (°C) from NKT modules."""
-    openPorts(port, 0, 0)  # Open port
+def read_powers_and_temp(
+    port='COM4',
+    harmonik_addr=74,
+):
+    openPorts(port, 0, 0)
 
-    # Read power (Harmonik-GW, register 0x9D, float32)
-    _, power_bytes = registerRead(port, harmonik_addr, 0x9D, -1)
-    power = struct.unpack('f', power_bytes[:4])[0]  # Ensure 4 bytes for float32
+    try:
+        # Harmonik input power
+        _, in_bytes = registerRead(port, harmonik_addr, 0x91, -1)
+        harmonik_input = struct.unpack('<f', in_bytes[:4])[0]
 
-    # Read temperature (Adjustik, register 0x11, I16 in m°C)
-    _, temp_bytes = registerRead(port, adjustik_addr, 0x11, -1)
-    temp = struct.unpack('h', temp_bytes[:2])[0] / 1000  # Ensure 2 bytes for I16
+        # Harmonik output power
+        _, out_bytes = registerRead(port, harmonik_addr, 0x9D, -1)
+        harmonik_output = struct.unpack('<f', out_bytes[:4])[0]
 
-    closePorts(port)  # Close port
-    return power, temp
+        # Harmonik temperature
+        _, temp_bytes = registerRead(port, harmonik_addr, 0xBA, -1)
+        harmonik_temp = struct.unpack('<f', temp_bytes[:4])[0]
+
+    finally:
+        closePorts(port)
+
+    return harmonik_input, harmonik_output, harmonik_temp
